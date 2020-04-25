d1ecd8a93dcefdfa994578bd07699d740ab52db9
takeshi.mito <mitomitotakeshi@gmail.com>
2020-04-25 09:25:28 +0000

    models setting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :prefecture
+  
+  validates :content, presence: true, length: { maximum: 255 }
+  validates :title, presence: true, length: { maximum: 50 }
 end
