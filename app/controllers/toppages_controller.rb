99448a150d3575ab572a3d289e38b3f58b85a661
takeshi.mito <mitomitotakeshi@gmail.com>
2020-05-22 14:04:51 +0000

    version 2

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,12 +1,21 @@
 class ToppagesController < ApplicationController
   def index
     @post = current_user.posts.build if logged_in?
+
     if (params[:prefecture_key])
-      @posts = Post.order(id: :desc).page(params[:page]).per(25).search(params[:content_key]).search_prefecture(params[:prefecture_key])
+      @posts = Post.order(id: :desc).page(params[:page]).per(25).search_prefecture(params[:prefecture_key])
     else
       @posts = Post.order(id: :desc).page(params[:page]).per(25).search(params[:content_key])
     end
+    
+    counts(@posts)
   end
   
-  
+  private  
+      
+  def counts(posts)
+    @count_posts = posts.count
+  end
 end
+  
+
